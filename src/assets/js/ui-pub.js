70396e5af21335d606145a3c36f96fb82b9b5aaa
이대성 <leeds019@naver.com>
2024-04-05 16:23:36 +0900

    init UI Update 0.14

--- src/assets/js/ui-pub.js
+++ src/assets/js/ui-pub.js
@@ -1,28 +1,46 @@
 const UI = {
     init : function () {
-        this.tooltips.init()
+        this.popup.init()
     },
-    tooltips : {
-        items: document.querySelectorAll("[data-tooltip]"),
-        itemArray: [],
+    // 공통 함수
+    activate : function ( elem ,className) {
+        elem.classList.add(className)
+    },
+    inActivate : function ( elem ,className) {
+        elem.classList.remove(className)
+    },
+    // 컴퍼넌트
+    popup : {
+        items : document.querySelectorAll("[data-popup]"),
+        itemArray : [],
         init : function () {
             this.items.length > 0 ?
                 this.dataCollection() :
-                (console.log("toggle func none"), false)
+                (console.log("popup func none"), false)
         },
-        dataCollection : function ( ){
+        dataCollection : function () {
             const _this = this;
             this.items.forEach((item) => {
-                if(_this.itemArray.indexOf(item.dataset.tooltip) === -1){
-                    _this.itemArray.push(item.dataset.tooltip)
+                if(_this.itemArray.indexOf(item.dataset.popup) === -1){
+                    _this.itemArray.push(item.dataset.popup)
                 }
             })
-
+            this.itemArray.length > 0 ?
+                this.events() : false
         },
         events : function () {
-            this.itemArray.forEach((item) => {
-                item.addEventListener("mouseenter", () => item.classList.add("is-show") )
-                item.addEventListener("mouseleave", () => item.classList.add("is-show") )
+            this.itemArray.forEach((dataName) => {
+                const popup = document.querySelector(`[data-popup=${dataName}]`)
+                const open = document.querySelector(`[data-popup-open=${dataName}]`)
+                const close = document.querySelector(`[data-popup-close=${dataName}]`)
+                popup.addEventListener("click", (e) => {
+                    console.log(e.target)
+                    if(e.target.classList.contains("flex-wrap")) {
+                        UI.inActivate(popup, "is-show")
+                    }
+                })
+                open.addEventListener("click", () => UI.activate(popup, "is-show"))
+                close.addEventListener("click", () => UI.inActivate(popup, "is-show"))
             })
         }
     }
